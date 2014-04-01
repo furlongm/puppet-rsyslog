a69e0ff113e4a4ee2303826bc8a6628b7543a4f7
Marcus Furlong <furlongm@gmail.com>
2014-04-02 10:54:40 +1100

    use new adiscon ppa repo, old repo is gone
    Change-Id: If367fa96aea29d174973193f7c31ac20907ba5f5

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -1,25 +1,11 @@
 class rsyslog::repos {
 
-  if $::rfc1918_gateway == 'true' {
-    exec { 'rsyslog-apt-key':
-     path        => '/usr/bin:/bin:/usr/sbin:/sbin',
-     command     => "apt-key adv --keyserver pgp.mit.edu --keyserver-options http-proxy=\"${::http_proxy}\" --recv-keys AEF0CF8E",
-     unless      => 'apt-key list | grep AEF0CF8E >/dev/null 2>&1',
-    }
-
-  } else {
-    apt::key { 'rsyslog':
-      key         => 'AEF0CF8E',
-      key_server => 'pgp.mit.edu',
-    }
-  }
-
   apt::source { 'rsyslog':
-    location     => 'http://ubuntu.adiscon.com/v7-stable',
-    release      => 'precise/',
-    repos        => ' ',
-    key          => 'AEF0CF8E',
+    location     => 'http://ppa.launchpad.net/adiscon/v7-stable/ubuntu/',
+    release      => "$::lsbdistcodename",
+    repos        => 'main',
     pin          => '2200',
+    key          => '5234BF2B',
     include_src  => false,
-  }
+ }
 }
