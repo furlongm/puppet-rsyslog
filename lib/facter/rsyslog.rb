c0e5425ab0d51498543d1ee6bb2c3a1fb8f5c339
Marcus Furlong <furlongm@gmail.com>
2014-04-09 12:19:15 +1000

    remove newline from rsyslog version fact
    Change-Id: I6c5b59aed82f26c7cf5db0ba0160e22aa0aa4f37

--- lib/facter/rsyslog.rb
+++ lib/facter/rsyslog.rb
@@ -1,7 +1,7 @@
 if FileTest.exists?("/usr/sbin/rsyslogd")
   Facter.add('rsyslog_majversion') do
     setcode do
-      %x{rsyslogd -v | head -n 1 | awk {'print $2'} | cut -d . -f 1}
+      %x{rsyslogd -v | head -n 1 | awk {'print $2'} | cut -d . -f 1}.chomp
     end
   end
 end
